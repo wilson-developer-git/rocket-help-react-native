6654897931b3f469c15a1d57ae9fbfa1f4d0e91a
wilson-developer-git <wilsonprogram@gmail.com>
2022-07-20 17:52:34 -0300

    Fonts adicionada e statusbar configurada

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 
-import SignIn from './src/screens/SignIn';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { SignIn } from './src/screens/SignIn';
+import {THEME} from './src/styles/theme';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  
   return (
-    <SignIn/>
+    <NativeBaseProvider theme={THEME}>
+
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      { fontsLoaded ? <SignIn /> : <Loading /> }
+    </NativeBaseProvider>
   );
 }
+
